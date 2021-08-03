382e9874fae1d5bc69a549c9c68af0178b956f96
Aayush <aayushkapoor12343@gmail.com>
2021-08-03 23:34:02 +0530

    created a folder card which contains images.

--- lib/screens/albums_op.dart
+++ lib/screens/albums_op.dart
@@ -1,3 +1,4 @@
+import 'package:camera_op/screens/image_view.dart';
 import 'package:flutter/material.dart';
 
 class AlbumsOp extends StatefulWidget {
@@ -15,40 +16,29 @@ class _AlbumsOpState extends State<AlbumsOp> {
         title: Text("ALBUMS"),
         backgroundColor: Colors.pink[800],
       ),
-      body: GridView.extent(
-        maxCrossAxisExtent: 150.0,
-        mainAxisSpacing: 5.0,
-        crossAxisSpacing: 5.0,
-        padding: const EdgeInsets.all(5.0),
-        children: _buildGridTiles(12),
-        //crossAxisCount: 3,
-        // children: List.generate(9, (index) {
-        //   return Center(
-        //     child: Card(
-        //       elevation: 8.0,
-        //       margin: EdgeInsets.all(7.0),
-        //       child: Image.network(
-        //       "https://png.pngtree.com/png-clipart/20201105/ourlarge/pngtree-orange-red-fire-ball-flame-clip-art-png-image_2388411.jpg",
-        //       ),
-        //     ),
-        //   );
-        // }),
-      ),
+      body: Center(
+        child: Column(
+          children: [
+            GestureDetector(
+              onTap: (){
+                ImageView();
+              },
+              child: Card(
+                elevation: 8.0,
+                color: Colors.amberAccent,
+                margin: EdgeInsets.all(10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Image.asset('images/folder2.jpg',width: 100,),
+                    Text("Folder Name"),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      )
     );
   }
-}
-List<Widget> _buildGridTiles(numberOfTiles) {
-  List<Container> containers = new List<Container>.generate(numberOfTiles,
-          (int index) {
-        //index = 0, 1, 2,...
-        final imageName = index < 9 ?
-        'images/image0${index + 1}.jpg' : 'images/image${index + 1}.jpg';
-        return new Container(
-          child: new Image.asset(
-              imageName,
-              fit: BoxFit.fill
-          ),
-        );
-      });
-  return containers;
 }
